6ea239bca594e17fad6d9653c50bac1fdea0198b
clia <clia@163.com>
2020-08-01 10:59:30 +0800

    change Exception process for Python3

--- manual-hook.py
+++ manual-hook.py
@@ -70,7 +70,7 @@ def auth():
         logger.info('DNS setting end!')
 
     except Exception as e:
-        logger.error('Error: ' + str(e.message) + '\n')
+        logger.error('Error: ' + e + '\n')
         sys.exit()
 
 
@@ -95,7 +95,7 @@ def cleanup():
         logger.info('Clean up end!')
 
     except Exception as e:
-        logger.error('Error: ' + str(e.message) + '\n')
+        logger.error('Error: ' + e + '\n')
         sys.exit()
 
 
@@ -170,8 +170,7 @@ def main(argc, argv):
     except AttributeError as e:
         logger.error(e.args)
     except Exception as e:
-        if e.message != '':
-            logger.error('Error: ' + str(e.message) + '\n')
+        logger.error('Error: ' + e + '\n')
 
         sys.exit()
 
